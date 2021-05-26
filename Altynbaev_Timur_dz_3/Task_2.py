e3dd9f60f6c0ff68a6c50eb836cdf5ef2ee214fd
Timur <altynbaevtimuru@yandex.ru>
2021-05-26 13:58:09 +0300

    Task 2

---
+++ Altynbaev_Timur_dz_3/Task_2.py
@@ -0,0 +1,16 @@
+words = {"zero": "ноль", "one": "один", "two": "два", "tree": "три", "four": "четыре", "five": "пять", "six": "шесть",
+         "seven": "семь", "eight": "восемь", "nine": "девять", "ten": "десять"}
+
+
+def num_translate(num):
+    words_keys = list(words.keys())
+    words_values = list(words.values())
+    for i in range(0, 11):
+        if num == str(words_keys[i]):
+            return words_values[i]
+        elif num == str(words_keys[i]).capitalize():
+            return str(words_values[i].capitalize())
+    return None
+
+
+print(num_translate((str(input("Напишите число от 0 до 10 прописными буквами ")))))
